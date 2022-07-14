d9573cb15f1ddd9b090c35eaff77abfe85a6c7c7
Pigeon2222 <109118138+Pigeon2222@users.noreply.github.com>
2022-07-14 12:50:10 +0900

    Add files via upload

---
+++ 조영탁/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            string pw = textBox2.Text;
+            MessageBox.Show("ID: " + id + ", PW: " + pw);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
